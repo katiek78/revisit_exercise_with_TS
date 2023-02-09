dad24b1fec5c2de1e45ef55c3be8ae62dae558df
katiek78 <kermodekatie@gmail.com>
2023-02-09 12:50:29 +0000

    Improved formatting of exercise.ts

--- src/exercise.test.ts
+++ src/exercise.test.ts
@@ -1,70 +1,73 @@
 import { getCities, User, areWeCovered, StaffSchedule, Day } from "./exercise";
 
 describe("getCities", () => {
-    test("returns an array of the cities of each user", () => {
-      const users: Array<User> = [
-        {
-          id: 12,
-          data: {
-            city: {
-              id: 1,
-              displayName: "MCR",
-            },
+  test("returns an array of the cities of each user", () => {
+    const users: Array<User> = [
+      {
+        id: 12,
+        data: {
+          city: {
+            id: 1,
+            displayName: "MCR",
           },
         },
-        {
-          id: 44,
-          data: {
-            city: {
-              id: 4,
-              displayName: "LVP",
-            },
+      },
+      {
+        id: 44,
+        data: {
+          city: {
+            id: 4,
+            displayName: "LVP",
           },
         },
-        {
-          id: 5,
-          data: {
-            city: {
-              id: 4,
-              displayName: "LVP",
-            },
+      },
+      {
+        id: 5,
+        data: {
+          city: {
+            id: 4,
+            displayName: "LVP",
           },
         },
-        {
-          id: 2,
-          data: {
-            city: {
-              id: 7,
-              displayName: "GLW",
-            },
+      },
+      {
+        id: 2,
+        data: {
+          city: {
+            id: 7,
+            displayName: "GLW",
           },
         },
-      ];
-      expect(getCities(users)).toEqual(["MCR", "LVP", "LVP", "GLW"]);
-    });
+      },
+    ];
+    expect(getCities(users)).toEqual(["MCR", "LVP", "LVP", "GLW"]);
   });
+});
 
-
-  const schedules1: Array<StaffSchedule> = [{ name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
-  { name: "Darwin", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
-  { name: "Virgil", rota: ["Wednesday"] }];
-
-  const schedules2: Array<StaffSchedule> = [{ name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
+const schedules1: Array<StaffSchedule> = [
+  { name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
   { name: "Darwin", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
   { name: "Virgil", rota: ["Wednesday"] },
-  { name: "Cody", rota: ["Wednesday", "Thursday"] }];
+];
 
-  describe("areWeCovered", () => {
-    test("returns true if at least 3 members of staff are working that day", () => {
-        expect(areWeCovered(schedules1, "Wednesday")).toBe(true);
-        expect(areWeCovered(schedules2, "Wednesday")).toBe(true);
-    });
-    test("returns false if 2 or fewer members of staff are working that day", () => {
-        expect(areWeCovered(schedules1, "Friday")).toBe(false);
-        expect(areWeCovered(schedules1, "Saturday")).toBe(false);
-        expect(areWeCovered(schedules1, "Tuesday")).toBe(false);
-    });
-    test("returns false if array is empty", () => {
-        expect(areWeCovered([], "Wednesday")).toBe(false);
-    });
+const schedules2: Array<StaffSchedule> = [
+  { name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
+  { name: "Darwin", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+  { name: "Virgil", rota: ["Wednesday"] },
+  { name: "Cody", rota: ["Wednesday", "Thursday"] },
+];
+
+describe("areWeCovered", () => {
+  test("returns true if at least 3 members of staff are working that day", () => {
+    expect(areWeCovered(schedules1, "Wednesday")).toBe(true);
+    expect(areWeCovered(schedules2, "Wednesday")).toBe(true);
+  });
+  test("returns false if 2 or fewer members of staff are working that day", () => {
+    expect(areWeCovered(schedules1, "Friday")).toBe(false);
+    expect(areWeCovered(schedules1, "Saturday")).toBe(false);
+    expect(areWeCovered(schedules1, "Tuesday")).toBe(false);
+  });
+  test("returns false if array is empty", () => {
+    expect(areWeCovered([], "Wednesday")).toBe(false);
+  });
 });
--- src/exercise.ts
+++ src/exercise.ts
@@ -1,31 +1,47 @@
-
-
-export type User = { id: number, data: { city: {id: number, displayName: string}}};
+export type User = {
+  id: number;
+  data: { city: { id: number; displayName: string } };
+};
 
 export function getCities(users: Array<User>) {
-    if (!users) throw new Error("users is required");
-    if (!Array.isArray(users)) throw new Error("users must be an array");
-  
-    return users.map(el => el.data?.city?.displayName);
-  }
+  if (!users) throw new Error("users is required");
+  if (!Array.isArray(users)) throw new Error("users must be an array");
 
-  export type StaffSchedule = {name: string, rota: Array<Day>};
-  export type Day = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
+  return users.map((el) => el.data?.city?.displayName);
+}
 
-  export const areWeCovered = (staff: Array<StaffSchedule>, day: Day) => {
-    if (staff === undefined) throw new Error("staff is required");
-    if (day === undefined) throw new Error("day is required");
-  
-    const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    if (DAYS.indexOf(day) === -1) throw new Error("day must be a valid day of the week");
-  
-    let count = 0;
-    for (let i = 0; i < staff.length; i++) {
-      if (staff[i].rota.includes(day)) {
-        count++;
-        if (count === 3) return true;
-      }
+export type StaffSchedule = { name: string; rota: Array<Day> };
+export type Day =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+export const areWeCovered = (staff: Array<StaffSchedule>, day: Day) => {
+  if (staff === undefined) throw new Error("staff is required");
+  if (day === undefined) throw new Error("day is required");
+
+  const DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  if (DAYS.indexOf(day) === -1)
+    throw new Error("day must be a valid day of the week");
+
+  let count = 0;
+  for (let i = 0; i < staff.length; i++) {
+    if (staff[i].rota.includes(day)) {
+      count++;
+      if (count === 3) return true;
     }
-    return false;
-  };
-  
+  }
+  return false;
+};
