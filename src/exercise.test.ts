c52a693c04e5a5904ad6a2110e1379314fb1c3d7
katiek78 <kermodekatie@gmail.com>
2023-02-09 12:49:48 +0000

    Implemented function areWeCovered and corresponding tests with TS

--- src/exercise.test.ts
+++ src/exercise.test.ts
@@ -1,4 +1,4 @@
-import { getCities, User } from "./exercise";
+import { getCities, User, areWeCovered, StaffSchedule, Day } from "./exercise";
 
 describe("getCities", () => {
     test("returns an array of the cities of each user", () => {
@@ -43,3 +43,28 @@ describe("getCities", () => {
       expect(getCities(users)).toEqual(["MCR", "LVP", "LVP", "GLW"]);
     });
   });
+
+
+  const schedules1: Array<StaffSchedule> = [{ name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
+  { name: "Darwin", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+  { name: "Virgil", rota: ["Wednesday"] }];
+
+  const schedules2: Array<StaffSchedule> = [{ name: "Jordan", rota: ["Monday", "Tuesday", "Wednesday"] },
+  { name: "Darwin", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+  { name: "Virgil", rota: ["Wednesday"] },
+  { name: "Cody", rota: ["Wednesday", "Thursday"] }];
+
+  describe("areWeCovered", () => {
+    test("returns true if at least 3 members of staff are working that day", () => {
+        expect(areWeCovered(schedules1, "Wednesday")).toBe(true);
+        expect(areWeCovered(schedules2, "Wednesday")).toBe(true);
+    });
+    test("returns false if 2 or fewer members of staff are working that day", () => {
+        expect(areWeCovered(schedules1, "Friday")).toBe(false);
+        expect(areWeCovered(schedules1, "Saturday")).toBe(false);
+        expect(areWeCovered(schedules1, "Tuesday")).toBe(false);
+    });
+    test("returns false if array is empty", () => {
+        expect(areWeCovered([], "Wednesday")).toBe(false);
+    });
+});
--- src/exercise.ts
+++ src/exercise.ts
@@ -8,3 +8,24 @@ export function getCities(users: Array<User>) {
   
     return users.map(el => el.data?.city?.displayName);
   }
+
+  export type StaffSchedule = {name: string, rota: Array<Day>};
+  export type Day = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
+
+  export const areWeCovered = (staff: Array<StaffSchedule>, day: Day) => {
+    if (staff === undefined) throw new Error("staff is required");
+    if (day === undefined) throw new Error("day is required");
+  
+    const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    if (DAYS.indexOf(day) === -1) throw new Error("day must be a valid day of the week");
+  
+    let count = 0;
+    for (let i = 0; i < staff.length; i++) {
+      if (staff[i].rota.includes(day)) {
+        count++;
+        if (count === 3) return true;
+      }
+    }
+    return false;
+  };
+  
